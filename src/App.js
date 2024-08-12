8f87874b3cecd76ae37bef4ad0d08dd82ed0922b
jasmithadodda <jasmithadodda@gmail.com>
2024-08-12 15:25:43 +0530

    buy given num of cakes -- adding a payload to the action string

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import CakeContainer from "./components/CakeContainer";
-import HooksCakeContainer from "./components/HooksCakeContainer";
+//import CakeContainer from "./components/CakeContainer";
+//import HooksCakeContainer from "./components/HooksCakeContainer";
+import  NewCakeContainer  from "./components/NewCakeContainer";
 // import DocTitleOne from "./hooks/DocTitleOne";
 // import DocTitleTwo from "./hooks/DocTitleTwo";
 //import CounterOne from "./hooks/CounterOne";
@@ -17,8 +18,8 @@ function App() {
         <DocTitleTwo/> */}
         {/* <CounterOne/>
         <CounterTwo/> */}
-        <CakeContainer/>
-        <HooksCakeContainer/>
+        <NewCakeContainer/>
+        {/* <HooksCakeContainer/> */}
       </div>
     </Provider>
   );
